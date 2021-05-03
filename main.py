c0f143c7f9ee90ee22bbc179413ef61e93e25466
Mesut Hakan Taşkıner <Hakantaskinerr@gmail.com>
2021-05-03 23:29:11 +0300

    Init Gui

---
+++ main.py
@@ -0,0 +1,107 @@
+from tkinter import *
+import random
+
+pressed_button = None
+starting_point = None
+obstacle_list = None
+destination_point = None
+
+def gui():
+    root = Tk()
+    root.title('QLearning Algorithm')
+    root.geometry('500x500')
+    root.resizable(width = FALSE, height = FALSE)
+    count = 0                                           # for identifying each button/vertex and passing unique parameters
+    button_list = []                                    # stores button created during runtime
+
+    matrix = [[0 for i in range(10)] for j in range(10)]
+
+    frame_up = Label(root)
+    frame_down = Label(root)
+    
+    frame_up.pack()
+    frame_down.pack()
+
+    global pressed_button                               # for differentiating b/w starting, ending & obstacles point
+    pressed_button = 0
+    global starting_point                               # starting_point is starting point
+    starting_point = 0
+    global obstacle_list                                # stores the obstacles when pressed_button is 2
+    obstacle_list = []
+    global destination_point                            # final destination variable
+    dest = 1000
+
+    def button_mode(mode):                              # input field by user starting/obstacles/destination point
+        global pressed_button
+        pressed_button = mode
+
+    def button_click(but_no):                                   # clicked buttons in path
+        global pressed_button
+
+        if pressed_button == 1:                                # for starting point when pressed_button = 1
+            button_list[but_no].config(bg = '#ffe525')
+            global starting_point
+            starting_point = but_no
+            start_button['state'] = DISABLED                    # Disable button after press
+            pressed_button = 0
+
+        if pressed_button == 2:                                # for destination when pressed_button = 2
+            button_list[but_no].config(bg = '#7dcf21')
+            global destination_point
+            destination_point = but_no
+            destination_button['state'] = DISABLED              # Disable button after press
+            pressed_button = 0
+
+    start_button = Button(frame_up, text = 'Select Start Point', command=lambda: button_mode(1))
+    destination_button = Button(frame_up, text = 'Select Destination', command=lambda: button_mode(2))
+
+    start_button.grid(row = 0, column = 0, padx = 10)
+    destination_button.grid(row = 0, column = 1)
+
+    for i in range(10):
+        for j in range(10):
+            random_number = random.randint(1,9)
+            button_list.append(Button(frame_down, text = f'{random_number}', padx = 5, pady = 5, command = lambda x=count: button_click(x)))
+            button_list[count].grid(row = i, column = j, sticky = "ew")
+            matrix[i][j] = random_number
+            count += 1
+
+    
+    '''def Run():                                         # algorithm script is called
+        parent = algorithm.backened(starting_point, obstacle_list, destination_point)
+        for value in parent:
+            button_list[value].config(bg = '#33fff0')         # path color is turned blue
+        button_list[starting_point].config(bg = '#ffe525')               # starting pt color is turned back yellow'''
+
+    #run_button = Button(frame_up, text = 'Run', command = Run)
+    #run_button.grid(row = 0, column = 2, padx = 10, pady = 5)
+
+   
+    def restart():           # Restarting Gui
+        root.destroy()
+        gui()
+        
+    restart_button = Button(frame_up, text='Restart', command = restart)
+    restart_button.grid(row = 0, column = 3, padx = 10, pady = 5)
+
+    def setting_obstacles(): # Creating obstacles that random place
+        global obstacle_list
+        random_obstacles_numbers = []
+        
+        for i in range(30): 
+            random_numbers = random.randint(0, 99)
+
+            while random_numbers in random_obstacles_numbers != True: # If there is random number in obstacle list generate new random number
+                random_numbers = random.randint(0, 99)
+
+            random_obstacles_numbers.append(random_numbers)
+        
+        obstacle_list = random_obstacles_numbers
+        for every in random_obstacles_numbers:
+            button_list[every].config(bg = '#ff8a33')
+
+    obstacle_button = Button(frame_up, text = 'Set Obstacles', command = setting_obstacles)
+    obstacle_button.grid(row = 0, column = 4, padx = 10, pady = 5)
+
+    mainloop()
+gui()
